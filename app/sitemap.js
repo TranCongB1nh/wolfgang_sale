58c8ac65508f81e83bb3e5d391f45a7b1eb0bcb4
TCB <tbinh2890@gmail.com>
2023-12-05 20:33:27 +0700

    sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,17 +1,17 @@
 import { fetchProducts } from "@/utils";
 
- 
-const URL = "https://mooment.vercel.app";
+
+const URL = "https://wolfgang.vercel.app";
 
 export default async function sitemap() {
   // const posts = getSortedPostsData.map(({ id, date }) => ({
   //   url: `${URL}/blog/${id}`,
   //   lastModified: date,
   // }));
-  const products = await fetchProducts(); 
-  const renderProducts =  products.map(({ _id, updatedAt }) => ({
-      url: `${URL}/${_id}`,
-      lastModified: updatedAt,
+  const products = await fetchProducts();
+  const renderProducts = products.map(({ _id, updatedAt }) => ({
+    url: `${URL}/${_id}`,
+    lastModified: updatedAt,
   }))
 
   const routes = ["", "/chi-nhanh", "/tai-khoan", "/tai-khoan?so-dia-chi", "/tai-khoan?don-hang", "/tai-khoan?yeu-thich", "/tai-khoan?da-xem"].map((route) => ({
